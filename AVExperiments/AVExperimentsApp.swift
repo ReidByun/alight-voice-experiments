a90a7c044e0adf9d7110c6568e8e9e7a5d98c9e2
temphee <temphee@gmail.com>
2022-06-04 10:50:25 +0900

    Initial Commit

---
+++ AVExperiments/AVExperimentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVExperimentsApp.swift
+//  AVExperiments
+//
+//  Created by Reid Byun on 2022/06/04.
+//
+
+import SwiftUI
+
+@main
+struct AVExperimentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
